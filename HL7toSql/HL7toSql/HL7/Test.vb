4f289eae733eed4952afaa284d41dfe9c79b8db1
Joás V. Pereira <1110166@isep.ipp.pt>
2015-03-17 17:36:19 +0000

    test de ligação a BD com o ManagerConfig

--- HL7toSql/HL7toSql/HL7/Test.vb
+++ HL7toSql/HL7toSql/HL7/Test.vb
@@ -1,3 +1,34 @@
-﻿Module Test
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+
+Module Test
+
+    Private Property myConn As SqlConnection
+
+    Private Property myCmd As SqlCommand
+
+    Private Property results As Object
+
+    Sub Main()
+        Dim strConn As String = ConfigurationManager.AppSettings("StrgConn").ToString
+        myConn = New SqlConnection(strConn)
+
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = "SELECT IdPaciente, Last_Name_Paciente FROM Paciente"
+        myConn.Open()
+
+        Dim myReader As SqlDataReader = myCmd.ExecuteReader()
+        Do While myReader.Read()
+            results = results & myReader.GetString(1) & ", " & myReader.GetInt32(0) & vbLf
+        Loop
+        'Display results.
+        MsgBox(results)
+        myConn.Close()
+
+    End Sub
+
+    Private Function myReader() As Object
+        Throw New NotImplementedException
+    End Function
 
 End Module
