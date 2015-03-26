f2f83e15a571012665b94b1199d18f4c31ac5adf
Joás V. Pereira <1110166@isep.ipp.pt>
2015-03-26 16:19:48 +0000

    limpesa de codigo

--- HL7toSql/HL7toSql/Database/MSSQLConnection.vb
+++ HL7toSql/HL7toSql/Database/MSSQLConnection.vb
@@ -43,54 +43,7 @@ Public Class MSSQLConnection
         Disconnect()
     End Sub
 
-    Public Sub execQuery(tableName As String, dataRow As DataRow)
-        Connect()
-        Try
-            Dim sqlQuery = "SELECT * FROM " & tableName
-            Dim dataAdapt As New SqlDataAdapter(sqlQuery, myConn)
-            Dim table As New DataTable
-            table.Rows.Add(dataRow)
-            table.AcceptChanges()
-            table.Dispose()
-        Catch ex As Exception
-            Console.WriteLine(ex.Message)
-        End Try
-        Disconnect()
-    End Sub
-
-    Public Function sendQuery(sqlQuery As String) As Object(,)
-        Dim tableReturn(,) As Object
-        Connect()
-        Try
-            Dim dataAdapt As New SqlDataAdapter(sqlQuery, myConn)
-            Dim table As New DataTable
-
-            'dataAdapt = New SqlDataAdapter(sqlQuery, myConn)
-            dataAdapt.Fill(table)
-
-            Dim columnsCount As Integer = table.Columns.Count - 1
-            Dim rowsCount As Integer = table.Rows.Count
-
-            Dim values(rowsCount, columnsCount) As Object
-
-            ' nome das colunas
-            For i = 0 To columnsCount Step 1
-                values(0, i) = table.Columns.Item(i).Caption
-            Next
-            For i = 0 To columnsCount Step 1
-                For j = 0 To columnsCount Step 1
-                    values(i + 1, j) = table.Rows(i).Item(j)
-                Next
-            Next
-            tableReturn = values
-        Catch ex As Exception
-
-        End Try
-        Disconnect()
-        Return tableReturn
-    End Function
-
-    Public Function sendQuery2(sqlQuery As String) As DataTable
+    Public Function sendQuery(sqlQuery As String) As DataTable
         'Dim tableReturn(,) As Object
         Connect()
         Dim table As New DataTable
