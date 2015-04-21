53c2ac490f2ae223621a778edf9ebd46366c363b
Tiago Ventura <ventiago@gmail.com>
2015-04-21 14:56:45 +0100

    MainFrom

---
+++ HL7toSql/HL7toSql/Forms/MainForm.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea3 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend3 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series5 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim Series6 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Connecttoserver = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cbGraph = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tbGraph = New System.Windows.Forms.TextBox()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.Chart1 = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(2, 6)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1050, 648)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Connecttoserver)
+        Me.TabPage1.Controls.Add(Me.DataGridView1)
+        Me.TabPage1.Controls.Add(Me.TextBox2)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1042, 622)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "HL7 To DB"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Connecttoserver
+        '
+        Me.Connecttoserver.Location = New System.Drawing.Point(6, 5)
+        Me.Connecttoserver.Name = "Connecttoserver"
+        Me.Connecttoserver.Size = New System.Drawing.Size(153, 27)
+        Me.Connecttoserver.TabIndex = 9
+        Me.Connecttoserver.Text = "Connect to Server"
+        Me.Connecttoserver.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(437, 38)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(596, 578)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AcceptsReturn = True
+        Me.TextBox2.Location = New System.Drawing.Point(6, 38)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(425, 578)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Label2)
+        Me.TabPage2.Controls.Add(Me.cbGraph)
+        Me.TabPage2.Controls.Add(Me.Button1)
+        Me.TabPage2.Controls.Add(Me.Label1)
+        Me.TabPage2.Controls.Add(Me.tbGraph)
+        Me.TabPage2.Controls.Add(Me.DataGridView2)
+        Me.TabPage2.Controls.Add(Me.Chart1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1042, 622)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Grafico"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(301, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Id SV :"
+        '
+        'cbGraph
+        '
+        Me.cbGraph.FormattingEnabled = True
+        Me.cbGraph.Location = New System.Drawing.Point(340, 6)
+        Me.cbGraph.Name = "cbGraph"
+        Me.cbGraph.Size = New System.Drawing.Size(68, 21)
+        Me.cbGraph.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(425, 7)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(87, 20)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "Pesquisa"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Paciente Id :"
+        '
+        'tbGraph
+        '
+        Me.tbGraph.Location = New System.Drawing.Point(81, 6)
+        Me.tbGraph.Name = "tbGraph"
+        Me.tbGraph.Size = New System.Drawing.Size(214, 20)
+        Me.tbGraph.TabIndex = 9
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(8, 43)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(337, 552)
+        Me.DataGridView2.TabIndex = 8
+        '
+        'Chart1
+        '
+        ChartArea3.Name = "ChartArea1"
+        Me.Chart1.ChartAreas.Add(ChartArea3)
+        Legend3.Name = "Legend1"
+        Me.Chart1.Legends.Add(Legend3)
+        Me.Chart1.Location = New System.Drawing.Point(364, 43)
+        Me.Chart1.Name = "Chart1"
+        Series5.ChartArea = "ChartArea1"
+        Series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line
+        Series5.Color = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Series5.Legend = "Legend1"
+        Series5.Name = "Series1"
+        Series6.ChartArea = "ChartArea1"
+        Series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point
+        Series6.Color = System.Drawing.Color.Maroon
+        Series6.Legend = "Legend1"
+        Series6.Name = "Series2"
+        Me.Chart1.Series.Add(Series5)
+        Me.Chart1.Series.Add(Series6)
+        Me.Chart1.Size = New System.Drawing.Size(673, 552)
+        Me.Chart1.TabIndex = 7
+        Me.Chart1.Text = "Chart1"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1051, 666)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "MainForm"
+        Me.Text = "MainForm"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Chart1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Connecttoserver As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cbGraph As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tbGraph As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+    Friend WithEvents Chart1 As System.Windows.Forms.DataVisualization.Charting.Chart
+End Class
---
+++ HL7toSql/HL7toSql/Forms/MainForm.vb
@@ -0,0 +1,103 @@
+﻿Imports System.IO
+Imports System.IO.StreamReader
+Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.Threading
+Imports System.Text
+Imports System.ComponentModel
+Imports System.Net.Sockets
+Imports System.Net
+Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class MainForm
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private da As SqlDataAdapter
+    Private ds As DataSet
+    Private listMsg As New List(Of Message)
+    Private c28 As Char = Chr(28)
+    Dim worker As BackgroundWorker = New BackgroundWorker
+    Private ReadOnly lock As New Object
+    Public Delegate Sub dataReceivedDelegate(ByVal TB As TextBox, ByVal txt As String)
+    Public Delegate Sub dbUpdateDelegate(ByVal DG As DataGridView)
+    Dim mp As New MindrayProtocol
+
+    Private controller As MSSQLControllerMindray
+#Region "HL72DB"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        controller = New MSSQLControllerMindray
+        showBDcontent(DataGridView1)
+        fillCBox()
+    End Sub
+    Private Sub showBDcontent(ByVal DG As DataGridView)
+        If DG.InvokeRequired Then
+            DG.Invoke(New dbUpdateDelegate(AddressOf showBDcontent), New Object() {DG})
+        Else
+            DG.DataSource = controller.getTable("Monitorizacao").Tables(0)
+            Me.DataGridView1.Columns(3).DefaultCellStyle.Format = "dd.MM.yyyy HH:mm:ss:fff"
+            Me.DataGridView1.Columns(4).DefaultCellStyle.Format = "dd.MM.yyyy HH:mm:ss:fff"
+        End If
+    End Sub
+
+    Private Sub Connecttoserver_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Connecttoserver.Click
+        mp.Connect()
+        AddHandler mp.OnReceiveMSG, AddressOf getData
+    End Sub
+
+    Private Sub getData(msg As Message)
+        dataReceived(TextBox2, msg.toString)
+        dataReceived(TextBox2, "")
+        showBDcontent(DataGridView1)
+    End Sub
+
+    Public Sub dataReceived(ByVal TB As TextBox, ByVal txt As String)
+        If TB.InvokeRequired Then
+            TB.Invoke(New dataReceivedDelegate(AddressOf dataReceived), New Object() {TB, txt})
+        Else
+            TB.AppendText(txt)
+            TB.AppendText(vbNewLine)
+        End If
+    End Sub
+    Private Sub MainForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        mp.Disconnect()
+    End Sub
+#End Region
+#Region "Grafico"
+
+    Private Sub fillCBox()
+
+        ' table As DataSet = controller.getTableCB("SinaisVitais").Tables(0)
+        For Each fill As DataRow In controller.getTableSV("SinaisVitais").Tables(0).Rows
+            cbGraph.Items.Add(fill("Descricao"))
+        Next
+
+    End Sub
+
+    Private Sub showBDcontentSearch(id As String)
+        Dim name2sv As DataSet = controller.getSVidFromName(cbGraph.SelectedItem.ToString)
+        Me.DataGridView2.DataSource = controller.getTableGraph(id).Tables(0)
+        Me.DataGridView2.Update()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        controller.setidPaciente(tbGraph.Text)
+        Dim name2sv As DataSet = controller.getSVidFromName(cbGraph.SelectedItem.ToString)
+        Dim id = name2sv.Tables(0).Rows(0).Item(0)
+        showBDcontentSearch(id)
+        Dim table As DataSet = controller.getTableGraph(id)
+        Chart1.Series(0).Points.Clear()
+        Chart1.Series(1).Points.Clear()
+        'Chart1.Series.Dispose()
+        Chart1.Series(1).ToolTip = cbGraph.SelectedItem.ToString & " = #VALY" & vbCrLf & "#AXISLABEL"
+        For Each value As DataRow In table.Tables(0).Rows
+            Dim i = Chart1.Series(0).Points.AddXY(value.Item(1).ToString, value.Item(0))
+            i = Chart1.Series(0).Points.AddXY(value.Item(2).ToString, value.Item(0))
+            Chart1.Series(1).Points.AddXY(value.Item(1).ToString, value.Item(0))
+            Chart1.Series(1).Points.AddXY(value.Item(2).ToString, value.Item(0))
+
+        Next
+        Chart1.DataBind()
+        Chart1.Update()
+    End Sub
+#End Region
+End Class
